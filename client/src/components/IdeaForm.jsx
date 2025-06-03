e0ec50351871a66a873e302ef4b789033b39b5d4
Anshuman <trivedianshuman77@gmail.com>
2025-06-04 00:44:26 +0530

    Initial commit with client and server

--- client/src/components/IdeaForm.jsx
+++ client/src/components/IdeaForm.jsx
@@ -10,7 +10,7 @@ const IdeaForm = () => {
     setIdea('');
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/generate-idea', {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/generate-idea`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ topic }),
